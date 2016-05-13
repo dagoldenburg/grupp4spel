b99a7af0e283a5a8f6b2d565886db389d10fa7b9
aigeth <aigeth.magendran@gmail.com>
2016-05-13 12:11:20 +0200

    Add files via upload

--- Alpha/main.h
+++ Alpha/main.h
@@ -1,19 +1,28 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-#define LEVEL_WIDTH 1280
-#define LEVEL_HEIGHT 960
+#include "SDL.h"
+
+
+#define LEVEL_WIDTH 1920
+#define LEVEL_HEIGHT 1088
 
 //Screen dimension constants
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 800
 #define TOTAL_TILES  192
 #define TOTAL_TILE_SPRITES 12
-#define PLAYER_SPEED 2
 #define TOTAL_COLLISION_TILES 2
-#define TILESIZE 80
-#define ROWS 16
-#define COLUMNS 12
-char recvbuffer[100];
+#define ROWS 60
+#define COLUMNS 34
+#define TILESIZE 32
+#define TILE_RED 2
+#define TILE_GREEN 1
+#define TILE_BLACK 0
 
-#endif // MAIN_H_INCLUDED
+extern SDL_Rect spriteFacing;
+
+
+
+
+#endif // GAMESTRUCT_H_INCLUDED
